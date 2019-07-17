e68c9ba12e7f9f703b7b2f6ff6ade3a8ccb694dc
Administrador <Administrador@PC-PC>
2019-07-17 12:43:23 -0300

    Criado servico para cadastro de grupo

---
+++ src/servico/GrupoService.java
@@ -0,0 +1,103 @@
+package servico;
+
+import controle.ConectaBanco;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import javafx.scene.control.Alert;
+import modelo.Grupo;
+
+public class GrupoService {
+
+    private final ConectaBanco conecta = new ConectaBanco();
+
+    public boolean salvar(Grupo grupo) {
+        if (this.conecta.conexao()) {
+            try {
+                String sql = "INSERT INTO LAPT51 (T51CDGRP,T51DSGRP) values(?,?)";
+                PreparedStatement pst = this.conecta.getConn().prepareStatement(sql);
+                pst.setString(1, grupo.getCodigo());
+                pst.setString(2, grupo.getNome());
+                pst.executeUpdate();
+                this.conecta.getConn().commit();
+                return true;
+            } catch (SQLException ex) {
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                try {
+                    this.conecta.getConn().rollback();
+                } catch (SQLException ex1) {
+                    alert.setTitle("Erro");
+                    alert.setContentText("Erro ao incluir Grupo");
+                    alert.show();
+                }
+                alert.setTitle("Erro");
+                alert.setContentText("Erro ao incluir Grupo");
+                alert.show();
+            }
+            this.conecta.desconecta();
+        }
+        return false;
+    }
+    
+    public boolean alterar(Grupo grupo) {
+        if (this.conecta.conexao()) {
+            try {
+                String sql = "update LAPT51 set T51DSGRP=? where T51CDGRP=?";
+                PreparedStatement pst = this.conecta.getConn().prepareStatement(sql);
+                pst.setString(1, grupo.getNome());
+                pst.setString(2, grupo.getCodigo());
+                pst.executeUpdate();
+                this.conecta.getConn().commit();
+                return true;
+            } catch (SQLException ex) {
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                try {
+                    this.conecta.getConn().rollback();
+                } catch (SQLException ex1) {
+                    alert.setTitle("Erro");
+                    alert.setContentText("Erro ao alterar Grupo");
+                    alert.show();
+                }
+                alert.setTitle("Erro");
+                alert.setContentText("Erro ao alterar Grupo");
+                alert.show();
+            }
+            this.conecta.desconecta();
+        }
+        return false;
+    }
+
+    public void excluirMovimento(Grupo grupo) {
+        if (this.conecta.conexao()) {
+            try {
+                String sql = "delete from LAPT51 where T51CDGRP=?";
+                PreparedStatement pst = this.conecta.getConn().prepareStatement(sql);
+                pst.setString(1, grupo.getCodigo());
+                pst.execute();
+                this.conecta.getConn().commit();
+            } catch (SQLException ex) {
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setTitle("Erro");
+                alert.setContentText("Erro ao excluir Grupo");
+                alert.show();
+            }
+        }
+    }
+
+    public List<Grupo> listarGrupos() throws SQLException {
+        List<Grupo> grupos = new ArrayList<>();
+        if (this.conecta.conexao()) {
+            String sql = "SELECT T51CDGRP as codigo,T51DSGRP as nome FROM LAPT51 ORDER BY T51DSGRP";
+            if (this.conecta.executaSQL(sql)) {
+                if (this.conecta.getRs().first()) {
+                    do {
+                        grupos.add(new Grupo(this.conecta.getRs().getString("codigo"), this.conecta.getRs().getString("nome")));
+                    } while (this.conecta.getRs().next());
+                    return grupos;
+                }
+            }
+        }
+        return grupos;
+    }
+}
