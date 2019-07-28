e1a409ccf4a6435b6466691c2bb9ba6933ab6c68
Eudes Batista <eudeskyo@hotmail.com>
2019-07-28 11:13:22 -0300

    Adicionado tela de filtro para relatrio de produtos e movimentação de estoque

--- src/controle/FrmRelatorioController.java
+++ src/controle/FrmRelatorioController.java
@@ -1,19 +1,28 @@
 package controle;
 
+import java.io.IOException;
 import java.net.URL;
 import java.sql.SQLException;
 import java.util.List;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 import modelo.dto.FiltroProduto;
 import relatorio.RelatorioProduto;
 import servico.MovimentoService;
@@ -57,6 +66,7 @@ public class FrmRelatorioController implements Initializable {
         this.listarEmpresa();
         this.btProdutos.setOnAction(evt -> this.imprimirRelatorioProduto());
         this.btMovimentacao.setOnAction(evt -> this.imprimirRelatorioEstoque());
+        this.btPesquisar.setOnAction(evt -> this.pesquisarProduto());
     }
 
     private void imprimirRelatorioProduto() {
@@ -74,6 +84,31 @@ public class FrmRelatorioController implements Initializable {
         this.filtroProduto.setDataFinal(this.dataFinal.getValue().toString());
         this.relatorioProduto.imprimirTodosProdutos(this.filtroProduto);
     }
+    
+    private void pesquisarProduto() {
+        try {
+            FXMLLoader fXMLLoader = new FXMLLoader(getClass().getResource("/visao/FrmPesquisa.fxml"));
+            Parent parent = fXMLLoader.load();
+            FrmPesquisaController controller = fXMLLoader.getController();
+            controller.editPesquisa.setText(this.editProduto.getText().trim().toUpperCase());
+            Scene scene = new Scene(parent);
+            Stage stage = new Stage(StageStyle.UNDECORATED);
+            stage.setScene(scene);
+            stage.initModality(Modality.APPLICATION_MODAL);
+            stage.showAndWait();
+            if (controller.tabela.getSelectionModel().getSelectedItem() != null) {
+                if (controller.isSelecionouRegistro()) {
+                    this.editProduto.setText(controller.tabela.getSelectionModel().getSelectedItem().getReferencia());
+                }
+            }
+        } catch (IOException ex) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Cadastro Rapido");
+            alert.setContentText("Erro ao carregar FrmPesquisa.");
+            alert.show();
+        }
+    }
+    
 
     private void listarEmpresa() {
         try {
