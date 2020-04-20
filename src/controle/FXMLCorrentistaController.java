0051b2db4c86cee4ae45f34533aee470e52aee4e
ZOOM <ZOOM@ZOOM-PC.mshome.net>
2020-04-20 14:21:42 -0300

    Criando conta corrente de cliente

--- src/controle/FXMLCorrentistaController.java
+++ src/controle/FXMLCorrentistaController.java
@@ -5,9 +5,18 @@
  */
 package controle;
 
+import com.jfoenix.controls.JFXDatePicker;
+import componentesjavafx.TextFieldCustom;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
 
 /**
  * FXML Controller class
@@ -16,9 +25,86 @@ import javafx.fxml.Initializable;
  */
 public class FXMLCorrentistaController implements Initializable {
 
-    /**
-     * Initializes the controller class.
-     */
+    
+       @FXML
+    private AnchorPane ancoraPrincipal;
+
+    @FXML
+    private AnchorPane ancoraSaldo;
+
+    @FXML
+    private Label labelSaldoDisponivel;
+
+    @FXML
+    private Label labelSaldoDevedor;
+
+    @FXML
+    private Label labelSaldoLimiteEmCredito;
+
+    @FXML
+    private Label labelTotalCredito;
+
+    @FXML
+    private Label labelTotalDebito;
+
+    @FXML
+    private JFXDatePicker textDataInicial;
+
+    @FXML
+    private JFXDatePicker textDataFinal;
+
+    @FXML
+    private Button btCredito;
+
+    @FXML
+    private Button btDebito;
+
+    @FXML
+    private TextField textPesquisa;
+
+    @FXML
+    private Button btDebito1;
+
+    @FXML
+    private AnchorPane ancoraListarClientes;
+
+    @FXML
+    private TableView<?> tabela;
+
+    @FXML
+    private TableColumn<?, ?> columnCodigo;
+
+    @FXML
+    private TableColumn<?, ?> columnCliente;
+
+    @FXML
+    private TableColumn<?, ?> columnLimiteCredito;
+
+    @FXML
+    private Button btSair;
+
+    @FXML
+    private Button btCredito1;
+
+    @FXML
+    private AnchorPane ancoraLancamento;
+
+    @FXML
+    private Label labelTipoLancamento;
+
+    @FXML
+    private TextFieldCustom textDescricao;
+
+    @FXML
+    private TextFieldCustom textValor;
+
+    @FXML
+    private Button btSalvar;
+
+    @FXML
+    private Button btVoltar;
+
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
